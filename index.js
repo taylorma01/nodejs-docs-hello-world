2749c967c9c41f076e8e3db046bcf0ebb90446b6
taylorma01 <35252583+taylorma01@users.noreply.github.com>
2019-02-15 18:02:11 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello South Africa!");
+    response.end("Well, Hello South Africa!");
 
 });
 
